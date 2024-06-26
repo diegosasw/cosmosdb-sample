24ce82576c7200f77769c115f9dbd108f7d872d4
Diego Martin <martind@vitessepsp.com>
2024-06-26 14:22:22 +0200

    Irrelevant SSL bypassing

--- src/CosmosDbSample/Program.cs
+++ src/CosmosDbSample/Program.cs
@@ -4,10 +4,29 @@ using Microsoft.Azure.Cosmos;
 const string accountEndpoint = "https://localhost:8081";
 const string authKeyOrResourceToken = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
+// IRRELEVANT
+// var cosmosClientOptions =
+//     new CosmosClientOptions
+//     {
+//         HttpClientFactory = () =>
+//         {
+//             var httpMessageHandler =
+//                 new HttpClientHandler
+//                 {
+//                     ServerCertificateCustomValidationCallback =
+//                         HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+//                 };
+//
+//             return new HttpClient(httpMessageHandler);
+//         },
+//         ConnectionMode = ConnectionMode.Gateway
+//     };
+
 var cosmosClient =
     new CosmosClient(
         accountEndpoint,
         authKeyOrResourceToken);
+        //,cosmosClientOptions);
 
 
 var sample = new Sample(cosmosClient);
